908150a478452455c55ded15c58156d4b62c32c2
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-11-17 22:02:29 -0600

    :sparkles: Opening and closing
    Both operations have been correctly implemented.

--- Open_Close.py
+++ Open_Close.py
@@ -22,7 +22,7 @@ import PIL as pil
 from utils import binarise, side_by_side
 
 
-# In[3]:
+# In[21]:
 
 
 def opening(src: np.ndarray, kernel: np.ndarray, iterations: int = 1) -> np.ndarray:
@@ -34,6 +34,18 @@ def opening(src: np.ndarray, kernel: np.ndarray, iterations: int = 1) -> np.ndar
             cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
     """
     return cv.dilate(cv.erode(src, kernel, iterations=iterations), kernel, iterations=iterations)
+##
+
+def closing(src: np.ndarray, kernel: np.ndarray, iterations: int = 1) -> np.ndarray:
+    """
+        As defined in pages 644 and 6 45 :
+            'The closing of A by B is simply the dilation of A by B, followed by erosion of the result by B.'
+        
+        This function is should be equivalent to :
+            cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
+    """
+    return cv.erode(cv.dilate(src, kernel, iterations=iterations), kernel, iterations=iterations)
+##
 
 
 # In[4]:
@@ -72,6 +84,8 @@ plt.imshow(binaria, cmap='gray')
 
 help(opening)
 
+
+# # Opening
 
 # In[14]:
 
@@ -115,8 +129,55 @@ kernel = np.ones((50, 2))
 side_by_side(binaria, cv.morphologyEx(binaria, cv.MORPH_OPEN, kernel), title1='Original', title2=f'cv.MORPH_OPEN with Kernel {kernel.shape}')
 
 
-# As Gonzalez explained on the book, an opening is nothing but an erosion followed by a dilation.
+# As Gonzalez explained in the book, an opening is nothing but an erosion followed by a dilation.
 # Our custom function ```opening(image, kernel)``` yields the same result as executing ```cv.morphologyEx(image, cv.MORPH_OPEN, kernel)```
+
+# # Closing
+
+# In[22]:
+
+
+kernel = np.ones((10, 10))
+side_by_side(binaria, closing(binaria, kernel), title1='Original', title2=f'closing() with Kernel {kernel.shape}')
+
+
+# In[23]:
+
+
+kernel = np.ones((10, 10))
+side_by_side(binaria, cv.morphologyEx(binaria, cv.MORPH_CLOSE, kernel), title1='Original', title2=f'cv.MORPH_CLOSE with Kernel {kernel.shape}')
+
+
+# In[24]:
+
+
+kernel = np.ones((2, 50))
+side_by_side(binaria, closing(binaria, kernel), title1='Original', title2=f'closing() with Kernel {kernel.shape}')
+
+
+# In[25]:
+
+
+kernel = np.ones((2, 50))
+side_by_side(binaria, cv.morphologyEx(binaria, cv.MORPH_CLOSE, kernel), title1='Original', title2=f'cv.MORPH_CLOSE with Kernel {kernel.shape}')
+
+
+# In[26]:
+
+
+kernel = np.ones((50, 2))
+side_by_side(binaria, closing(binaria, kernel), title1='Original', title2=f'closing() with Kernel {kernel.shape}')
+
+
+# In[27]:
+
+
+kernel = np.ones((50, 2))
+side_by_side(binaria, cv.morphologyEx(binaria, cv.MORPH_CLOSE, kernel), title1='Original', title2=f'cv.MORPH_CLOSE with Kernel {kernel.shape}')
+
+
+# As Gonzalez explained on the book, the opening operation is nothing but a dilation followed by an erosion.
+# Our custom function ```closing(image, kernel)``` yields the same result as executing ```cv.morphologyEx(image, cv.MORPH_CLOSE, kernel)```
 
 # # Example found on page 643
 
